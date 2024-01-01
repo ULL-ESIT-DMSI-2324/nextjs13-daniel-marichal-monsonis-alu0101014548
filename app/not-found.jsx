8964475506cc2688fd51b66f6781c572f9346fe9
Dan <114574967+DanMAMO@users.noreply.github.com>
2024-01-01 23:27:49 +0000

    Add NotFound components for 404 pages

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
-export default function notFound() {
+import Link from "next/link"
+export default function NotFound() {
   return (
     <main className="center">
-        <h2 className='text-center'>Page not found</h2>
+        <h2 className="text-center">Page not found</h2>
         <p>Are you lost?</p>
         <p>Go back to the <Link href="/">Dashboard</Link></p>
     </main>
