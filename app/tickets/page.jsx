552963316361d877585958f85d2a072b07f9b8f7
Dan <114574967+DanMAMO@users.noreply.github.com>
2024-01-01 23:42:10 +0000

    Add Suspense and Loading component to Tickets page

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import TicketList from './TicketList'
+import Loading from '../loading'
 
 export default function Tickets() {
   return (
     <main>
       <nav>
         <div>
-          <h2>
+          <h2>Tickets</h2>
             <p><small>Currently open tickets.</small></p>
-          </h2>
+          
         </div>
       </nav>
 
-      <TicketList />
+      <Suspense fallback={<Loading />}>
+        <TicketList />
+      </Suspense>
+      
     </main>
   )
 }
