f89e5e6e5932ae25adbeffeca15199ffd93a0da7
Dan <114574967+DanMAMO@users.noreply.github.com>
2024-01-01 23:27:38 +0000

    Fix API endpoint and link URL in TicketList.jsx

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,13 +1,12 @@
 import Link from 'next/link';
-import React from 'react'
 async function getTickets() {
     const response = await fetch('http://localhost:4000/tickets', {
         next: {
                 revalidate: 0, // refresh every second
         }
     });
-    const data = await response.json();
-    return data;
+    
+    return response.json()
 }
 
 export default async function TicketList() {
@@ -17,7 +16,7 @@ export default async function TicketList() {
     <>
         {tickets.map((ticket) => (
             <div key={ticket.id} className="card my-5">
-                <Link href={`/tickets/@{ticket.id}`}>
+                <Link href={`/tickets/${ticket.id}`}>
                     <h3>{ticket.title}</h3>
                     <p>{ticket.body.slice(0, 200)}...</p>
                     <div className={`pill ${ticket.priority}`}>
