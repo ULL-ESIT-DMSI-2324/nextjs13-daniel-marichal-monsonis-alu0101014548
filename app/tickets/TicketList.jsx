d88eb4e59f4728d9e1adaee1893b51e23b00fd91
Dan <114574967+DanMAMO@users.noreply.github.com>
2024-01-01 21:29:15 +0000

    Add link to ticket details in TicketList component

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 async function getTickets() {
     const response = await fetch('http://localhost:4000/tickets', {
@@ -14,15 +15,20 @@ export default async function TicketList() {
     const tickets = await getTickets();
   return (
     <>
-        {tickets.map(ticket => (
+        {tickets.map((ticket) => (
             <div key={ticket.id} className="card my-5">
-                <h3>{ticket.title}</h3>
-                <p>{ticket.body.slice(0, 200)}...</p>
-                <div className= {'pill ${ticket.priority} '}></div>
-                {ticket.priority} priority
+                <Link href={'/tickets/@{ticket.id}'}>
+                    <h3>{ticket.title}</h3>
+                    <p>{ticket.body.slice(0, 200)}...</p>
+                    <div className= {'pill ${ticket.priority} '}>
+                        {ticket.priority} priority
+                    </div>
+                </Link>
             </div>
         ))}
-        {tickets.length === 0 && <p>No tickets to show</p>}
+        {tickets.length === 0 && (
+            <p className= "text-center" > There are no open tickets, geronimo!</p>
+        )}
     </>
   )
 }
